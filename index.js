d8decc4c6d23ccbe84ff044dbfeccddbe4912c48
Funds7 <odigwejoshua7@gmail.com>
2026-04-21 13:52:31 +0100

    Serve static files and redirect to login.html
    Add path module and serve static files from 'public' directory.

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.redirect("/login.html");
+});
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
