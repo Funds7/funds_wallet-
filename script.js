cafb5b6e7051d81c03ba34dbf659329db3586352
Funds7 <odigwejoshua7@gmail.com>
2026-04-23 02:35:33 +0100

    Implement fake price engine and transaction functions
    Added a fake price engine with buy and sell functions to simulate transactions.

--- script.js
+++ script.js
@@ -38,3 +38,48 @@ function logout(){
   localStorage.clear();
   window.location.replace("./index.html");
 }
+// FAKE PRICE ENGINE
+let price = 65000;
+let balance = 1000;
+
+function updatePrice(){
+  price += (Math.random() - 0.5) * 500;
+
+  const el = document.getElementById("price");
+  if(el) el.innerText = price.toFixed(2);
+}
+
+setInterval(updatePrice, 2000);
+
+// BUY FUNCTION
+function buy(){
+  if(balance < 100){
+    alert("Not enough balance");
+    return;
+  }
+
+  balance -= 100;
+
+  const el = document.getElementById("balance");
+  if(el) el.innerText = balance;
+
+  addHistory("Bought BTC at " + price.toFixed(2));
+}
+
+// SELL FUNCTION
+function sell(){
+  balance += 100;
+
+  const el = document.getElementById("balance");
+  if(el) el.innerText = balance;
+
+  addHistory("Sold BTC at " + price.toFixed(2));
+}
+
+// HISTORY
+function addHistory(text){
+  const div = document.getElementById("history");
+  const p = document.createElement("p");
+  p.innerText = text;
+  div.prepend(p);
+}
